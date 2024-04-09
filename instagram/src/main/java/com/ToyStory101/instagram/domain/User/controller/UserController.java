095515e04cbbca82d7912b5fd4ce33162a548fd9
Gongchaeyeon <dnjftjd53@kookmin.ac.kr>
2024-04-10 04:54:56 +0900

    [feat] 로그아웃 기능 추가

--- instagram/src/main/java/com/ToyStory101/instagram/domain/User/controller/UserController.java
+++ instagram/src/main/java/com/ToyStory101/instagram/domain/User/controller/UserController.java
@@ -6,9 +6,12 @@ import com.ToyStory101.instagram.domain.User.entity.User;
 import com.ToyStory101.instagram.domain.User.service.UserService;
 import com.ToyStory101.instagram.global.dto.CODE;
 import com.ToyStory101.instagram.global.dto.Result;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +45,27 @@ public class UserController {
 
         HttpSession session = request.getSession(true);
         session.setAttribute(user.getName(), user.getPassword());
+        return ResponseEntity.ok().body(result);
+
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session = request.getSession(false);
+
+        if(session != null){ // null이 아니라는건 기존에 세션이 존재했었다는 뜻이므로
+            session.invalidate();
+        }
+
+//        Cookie myCookie = new Cookie("JSESSIONID",null);  // 쿠키 값을 null로 설정
+//        myCookie.setMaxAge(0);
+//        response.addCookie(myCookie);
+
+        Result result = new Result();
+        result.setCode(CODE.SUCCESS);
+        result.setMessage("로그아웃 완료");
+        result.setData(null);
+
         return ResponseEntity.ok().body(result);
 
     }
